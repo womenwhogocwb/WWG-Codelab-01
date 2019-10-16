170a47608049bd02f1628dbf5a5914f370eb5e06
danditeoa <taci.dandi@gmail.com>
2019-10-16 13:14:32 -0300

    exercicios(codelab-dia-1): create solution for 1, 2 , 3, 4

--- codelab_dia_01/main.go
+++ codelab_dia_01/main.go
@@ -1,5 +1,54 @@
 package main
 
+import "fmt"
+
 func main() {
-	println("coloque suas resoluções nesse arquivo :D")
+
+	//ex1
+	var a = ola;
+	var b = pessoas;
+	var c = bonitas;
+	fmt.Println(a, b, c);
+
+	//ex2
+	a, b := 230, 27
+	fmt.Println("A: ", a)
+	fmt.Println("B: ", b)
+
+	soma := a + b
+	subtrai := a - b
+	fmt.Println("Soma: ", soma)
+	fmt.Println("Subtração: ", subtrai)
+
+	//ex3
+	a, b := 230, 27
+	fmt.Println("A: ", a)
+	fmt.Println("B: ", b)
+
+	soma := a + b
+	subtrai := a - b
+	multiplica := (a * b)
+	divide := (a/ b);
+	
+	fmt.Println("Soma: ", soma)
+	fmt.Println("Subtração: ", subtrai)
+	fmt.Println("Multipuicação: ", multiplica)
+	fmt.Println("Divisão: ", divide)
+
+	//ex4
+	a, b := 230, 27
+	fmt.Println("A: ", a)
+	fmt.Println("B: ", b)
+
+	soma := a + b
+	subtrai := a - b
+	multiplica := (a * b)
+	divide := (a/ b);
+	
+	fmt.Println("Soma: ", soma)
+	fmt.Println("Subtração: ", subtrai)
+	fmt.Println("Multipuicação: ", multiplica)
+	fmt.Println("Divisão: ", divide)
+	
+	fmt.Printf("\n%v \n%v  \n%v  \n%v", soma, subtrai, multiplica, divide)
 }
